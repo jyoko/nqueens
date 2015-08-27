f1c28bed09b70367f4296d765a32e5aba9b8a8e5
jyoko <jyoko@users.noreply.github.com>
2015-08-27 11:23:29 -0400

    Added some functions
    * Mostly spent time discussing math involved
    * Flaw found in testing set (doesn't check all potential conflicts)
    * Current code, particularly for queens, not heavily checked

--- src/solvers.js
+++ src/solvers.js
@@ -14,9 +14,20 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({n:n});
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  for (var i=0; i<n; i++) {
+    for (var j=0; j<n; j++) {
+      j = j+i;
+      board.togglePiece(i,j);
+      if (board.hasAnyRooksConflicts()) {
+        board.togglePiece(i,j);
+        break;
+      }
+    }
+  }
+  var solution = board.rows();
+//  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
@@ -24,9 +35,56 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  // var solutionCount = 0;
+  // var positions = [[]];
+  // var solved = false;
+  // var startingI = 0;
+  // var startingJ = 0;
+  var arr = [];
+  for(var i = n; i > 0; i--){
+    arr.push(i);
+  }
+  var solutionCount = _.reduce(arr, function(acc, val){
+    acc = acc * val;
+    return acc;
+  });
+  
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  // var generateSolutionCount = function(n,solutionCount) {
+/*with this approach are we attempting to create an array of 
+all matrices in order to offer all possible solutions as the final
+result?*/
+// we can just return the result count
+
+    //rows left to search
+  //   var availableRows;
+  //   //for the first row
+  //   if (currentRow===0) {
+  //     //set our available rows to n less 1 : never hit last one
+  //     var availableRows = n-1;
+  //   }
+  //   //if our current row is less than half (rounded up for odds)
+  //   if (currentRow<Math.ceil(n/2)) {
+  //     //rows left to run through is 'half'
+  //     availableRows = Math.ceil(n/2);
+  //   }
+
+  //   //if there are no more avail positions, we are at end of currentRow possibilites, move to next row
+  //   if (!availablePositions) {
+  //     //increment count
+  //     currentRow++;
+  //     return;
+  //   }
+
+  //   for (var i=0;i<availableRows;i++) {
+  //     // set initial value
+  //     //
+  //     // recurse(inital_row)
+  //   }
+
+  // };
+  
+//  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
@@ -34,7 +92,27 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({n:n});
+  var even = (n%2===0)?true:false;
+  var innerStart = (even)?1:0;
+  for (var i=0; i<n; i++) {
+    for (var j=innerStart; j<n; j++) {
+      j = j+(i*2);
+      if (even) {
+        if ( i===((n/2)-1) )  {
+          innerStart=;
+        }
+      }
+      if (j>=n) {
+        j = j-n;
+        // i++;
+      }
+      console.log(n+' board toggle: '+i+','+j);
+      board.togglePiece(i,j);
+      break;
+    }
+  }
+  var solution = board.rows();
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
